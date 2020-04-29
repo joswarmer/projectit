db7c7bd86ca94e22bac1355fb27474047d964eb2
Jos Warmer <jos.warmer@openmodeling.nl>
2020-04-29 15:46:18 +0200

    Generate default editors improved

--- packages/meta/src/editordef/metalanguage/DefEditorChecker.ts
+++ packages/meta/src/editordef/metalanguage/DefEditorChecker.ts
@@ -23,7 +23,7 @@ import { PiLogger } from "../../../../core/src/util/PiLogging";
 // Otherwise, the run-time error 'Cannot read property 'create' of undefined' occurs.
 // See: https://stackoverflow.com/questions/48123645/error-when-accessing-static-properties-when-services-include-each-other
 // and: https://stackoverflow.com/questions/45986547/property-undefined-typescript
-import { PiElementReference} from "../../languagedef/metalanguage/PiElementReference";
+import { PiElementReference } from "../../languagedef/metalanguage/PiElementReference";
 
 const LOGGER = new PiLogger("DefEditorChecker"); //.mute();
 
@@ -41,14 +41,14 @@ export class DefEditorChecker extends Checker<DefEditorLanguage> {
      * @param editor
      */
     public check(editor: DefEditorLanguage): void {
-        if ( this.language === null || this.language === undefined ) {
+        if (this.language === null || this.language === undefined) {
             throw new Error(`Editor definition checker does not known the language.`);
         }
 
         this.nestedCheck(
             {
                 check: this.language.name === editor.languageName,
-                error:  `Language reference ('${editor.languageName}') in editor definition '${editor.name}' `+
+                error: `Language reference ('${editor.languageName}') in editor definition '${editor.name}' ` +
                     `does not match language '${this.language.name}' [line: ${editor.location?.start.line}, column: ${editor.location?.start.column}].`,
                 whenOk: () => {
                     this.resolveReferences(editor);
@@ -58,7 +58,7 @@ export class DefEditorChecker extends Checker<DefEditorLanguage> {
                             check: !!editor.name,
                             error: `Editor should have a name, it is empty [line: ${editor.location?.start.line}, column: ${editor.location?.start.column}].`
                         });
-                    for(let conceptEditor of editor.conceptEditors){
+                    for (let conceptEditor of editor.conceptEditors) {
                         this.checkConceptEditor(conceptEditor);
                     }
                     this.errors = this.errors.concat(this.myExpressionChecker.errors);
@@ -74,7 +74,7 @@ export class DefEditorChecker extends Checker<DefEditorLanguage> {
             error: `Concept ${conceptEditor.concept.name} is unknown [line: ${conceptEditor.location?.start.line}, column: ${conceptEditor.location?.start.column}].`,
             whenOk: () => {
                 this.checkProjection(conceptEditor.projection, conceptEditor.concept.referred);
-            },
+            }
         });
     }
 
@@ -99,18 +99,28 @@ export class DefEditorChecker extends Checker<DefEditorLanguage> {
 
     private addDefaults(editor: DefEditorLanguage) {
         for (let con of this.language.concepts.filter(c => !(c instanceof PiBinaryExpressionConcept))) {
-            if (!editor.conceptEditors.map((ce) => ce.concept.referred).includes(con)) {
-                console.log("=============== adding default p0rojection for "+ con.name);
-                const coneditor = new DefEditorConcept();
+            // Find or create the concept editor, and its properties
+            let coneditor: DefEditorConcept = editor.conceptEditors.find(ed => ed.concept.referred === con);
+            if (!coneditor) {
+                coneditor = new DefEditorConcept();
                 coneditor.concept = PiElementReference.create<PiConcept>(con.name, "PiConcept");
                 coneditor.concept.owner = this.language;
                 coneditor.languageEditor = editor;
-                coneditor.symbol = con.name;
+                editor.conceptEditors.push(coneditor);
+            }
+            if(!coneditor.trigger){
                 coneditor.trigger = con.name;
+            }
+            if(!coneditor.symbol){
+                coneditor.symbol = con.name;
+            }
+            if( ! coneditor.projection) {
+                // create a new projection
+                console.log("Adding deafult projection for "+ con.name)
                 coneditor.projection = new MetaEditorProjection();
                 coneditor.projection.name = "default";
                 coneditor.projection.conceptEditor = coneditor;
-                for(let prop of con.allPrimProperties()){
+                for (let prop of con.allPrimProperties()) {
                     const line = new MetaEditorProjectionLine();
                     line.indent = 0;
                     line.items.push(DefEditorProjectionText.create(prop.name));
@@ -121,33 +131,22 @@ export class DefEditorChecker extends Checker<DefEditorLanguage> {
                     line.items.push(sub);
                     coneditor.projection.lines.push(line);
                 }
-                // for(let prop of con.allEnumProperties()){
-                //     const line = new MetaEditorProjectionLine();
-                //     line.indent = 0;
-                //     line.items.push(DefEditorProjectionText.create(prop.name))
-                //     const exp = new PiLangSelfExp.create(con);
-                //     exp.appliedfeature = PiLangAppliedFeatureExp.create(exp, prop.name, prop);
-                //     const sub = new DefEditorSubProjection();
-                //     sub.expression = exp;
-                //     line.items.push(sub);
-                //     coneditor.projection.lines.push(line);
-                // }
-                for(let prop of con.allParts()){
-                    if(prop.isList) {
+                for (let prop of con.allParts()) {
+                    if (prop.isList) {
                         this.defaultListConceptProperty(con, prop, coneditor);
                     } else {
                         this.defaultSingleConceptProperty(con, prop, coneditor);
                     }
                 }
-                for(let prop of con.allReferences()){
-                    if(prop.isList) {
+                for (let prop of con.allReferences()) {
+                    if (prop.isList) {
                         this.defaultListConceptProperty(con, prop, coneditor);
                     } else {
-                        this.defaultSingleConceptProperty(con, prop,coneditor);
+                        this.defaultSingleConceptProperty(con, prop, coneditor);
                     }
                 }
-                editor.conceptEditors.push(coneditor);
             }
+
         }
     }
 
