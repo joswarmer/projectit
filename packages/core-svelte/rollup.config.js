c2489dc0fc1747575f1ad017862c3b9aa4567d4e
jos <jos.warmer@openmodeling.nl>
2021-07-11 14:57:34 +0200

    emitCCC to get6 css from core-svelet in imported library

--- packages/core-svelte/rollup.config.js
+++ packages/core-svelte/rollup.config.js
@@ -35,11 +35,12 @@ export default {
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
-			}
+			},
+			emitCss: true
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: 'core-svelte.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
